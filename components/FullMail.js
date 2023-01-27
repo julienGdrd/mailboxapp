b6d2cc3f66ee29b3646c20086321c4ea87fd34c3
Julien Goudard <juliengoudard.g@gmail.com>
2023-01-27 09:47:03 +0100

    cleaning

--- components/FullMail.js
+++ components/FullMail.js
@@ -1,60 +1,85 @@
-import styles from '../styles/FullMail.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faBars, faCaretDown, faCircle, faCircleChevronDown, faCircleInfo, faEllipsisVertical, faGear, faInbox, faMagnifyingGlass, faPaperclip, faPen, faPlus, faReply, faRotateRight, faSliders, faTag, faUser, faUserGroup } from '@fortawesome/free-solid-svg-icons';
-import { faBookmark, faClock, faFile, faSquare, faStar } from '@fortawesome/free-regular-svg-icons';
+import styles from "../styles/FullMail.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleRight,
+  faBars,
+  faCaretDown,
+  faCircle,
+  faCircleChevronDown,
+  faCircleInfo,
+  faEllipsisVertical,
+  faGear,
+  faInbox,
+  faMagnifyingGlass,
+  faPaperclip,
+  faPen,
+  faPlus,
+  faReply,
+  faRotateRight,
+  faSliders,
+  faTag,
+  faUser,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBookmark,
+  faClock,
+  faFile,
+  faSquare,
+  faStar,
+} from "@fortawesome/free-regular-svg-icons";
 
 function FullMail(props) {
-    return (
-        <div className={styles.mainMessageContainer}>
-            <div className={styles.objectContainer}>
-                {props.object}
-                <div className={styles.objectIconContainer}>
-                    <FontAwesomeIcon
-                        className={styles.objectIcon}
-                        icon={faBookmark}
-                    />
-                </div>
-            </div>
-            <div className={styles.contentContainer}>
-                <div className={styles.leftColumn}>
-
-                </div>
-                <div className={styles.rightContainer}>
-                    <div className={styles.autorInfos}>
-                        <div className={styles.autorInfosUpPart}>
-                            <div className={styles.nameContainer}>
-                                {props.autor}
-                            </div>
-                            <div className={styles.rightDateControlsContainer}>
-                                <div className={styles.deliveryDate}>{props.deliveryDate} 2023 12:00</div>
-                                <div className={styles.controlIconsRow}>
-                                    <div className={styles.controlIconContainer}><FontAwesomeIcon
-                                        className={styles.controlIcon}
-                                        icon={faStar}
-                                    />
-                                    </div>
-                                    <div className={styles.controlIconContainer}><FontAwesomeIcon
-                                        className={styles.controlIcon}
-                                        icon={faReply}
-                                    />
-                                    </div>
-                                    <div className={styles.controlIconContainer}><FontAwesomeIcon
-                                        className={styles.controlIcon}
-                                        icon={faEllipsisVertical}
-                                    />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.autorInfosDownPart}>
-                            <span>À </span><span>moi</span>
-                        </div>
-                    </div>
-                    <div className={styles.textContent}>{props.content}</div>
-                </div>
-            </div>
+  return (
+    <div className={styles.mainMessageContainer}>
+      <div className={styles.objectContainer}>
+        {props.object}
+        <div className={styles.objectIconContainer}>
+          <FontAwesomeIcon className={styles.objectIcon} icon={faBookmark} />
         </div>
-    )
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.leftColumn}></div>
+        <div className={styles.rightContainer}>
+          <div className={styles.autorInfos}>
+            <div className={styles.autorInfosUpPart}>
+              <div className={styles.nameContainer}>{props.autor}</div>
+              <div className={styles.rightDateControlsContainer}>
+                <div className={styles.deliveryDate}>
+                  {props.deliveryDate} 2023 12:00
+                </div>
+                <div className={styles.controlIconsRow}>
+                  <div className={styles.controlIconContainer}>
+                    <FontAwesomeIcon
+                      className={styles.controlIcon}
+                      icon={faStar}
+                    />
+                  </div>
+                  <div className={styles.controlIconContainer}>
+                    <FontAwesomeIcon
+                      className={styles.controlIcon}
+                      icon={faReply}
+                    />
+                  </div>
+                  <div className={styles.controlIconContainer}>
+                    <FontAwesomeIcon
+                      className={styles.controlIcon}
+                      icon={faEllipsisVertical}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.autorInfosDownPart}>
+              <span>À </span>
+              <span>moi</span>
+            </div>
+          </div>
+          <div className={styles.textContent}>{props.content}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FullMail;
--- components/Home.js
+++ components/Home.js
@@ -1,22 +1,47 @@
-import styles from '../styles/Home.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faBars, faCaretDown, faCircle, faCircleChevronDown, faCircleInfo, faEllipsisVertical, faGear, faInbox, faMagnifyingGlass, faPaperclip, faPen, faPlus, faRotateRight, faSliders, faTag, faUser, faUserGroup } from '@fortawesome/free-solid-svg-icons';
-import { faBookmark, faClock, faFile, faSquare, faStar } from '@fortawesome/free-regular-svg-icons';
-import RowMail from '../components/Mail';
-import FullMail from './FullMail';
+import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleRight,
+  faBars,
+  faCaretDown,
+  faCircle,
+  faCircleChevronDown,
+  faCircleInfo,
+  faEllipsisVertical,
+  faGear,
+  faInbox,
+  faMagnifyingGlass,
+  faPaperclip,
+  faPen,
+  faPlus,
+  faRotateRight,
+  faSliders,
+  faTag,
+  faUser,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBookmark,
+  faClock,
+  faFile,
+  faSquare,
+  faStar,
+} from "@fortawesome/free-regular-svg-icons";
+import RowMail from "../components/Mail";
+import FullMail from "./FullMail";
 
-import { useSelector, useDispatch } from 'react-redux';
-import { resetMailToDisplay } from '../reducers/mailDisplayer';
+import { useSelector, useDispatch } from "react-redux";
+import { resetMailToDisplay } from "../reducers/mailDisplayer";
 
 function Home() {
-
   const dispatch = useDispatch();
   const mailList = [
     {
       autor: "Aliénor",
       emailAdress: "fake@gmail.com",
       object: "Résultat de ton passage du titre",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
       deliveryDate: "19 janv.",
       unRead: true,
       important: false,
@@ -24,13 +49,14 @@ function Home() {
       spam: false,
       pro: false,
       perso: false,
-      categorie: '',
+      categorie: "",
     },
     {
       autor: "Jhon",
       emailAdress: "Jhon@gmail.com",
       object: "Offre exceptionnelle",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
       deliveryDate: "17 janv.",
       unRead: true,
       important: false,
@@ -38,13 +64,14 @@ function Home() {
       spam: false,
       pro: false,
       perso: false,
-      categorie: '',
+      categorie: "",
     },
     {
       autor: "Martin",
       emailAdress: "martin@gmail.com",
       object: "Weekend ski",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
       deliveryDate: "12 janv.",
       unRead: true,
       important: false,
@@ -52,13 +79,14 @@ function Home() {
       spam: false,
       pro: false,
       perso: false,
-      categorie: '',
+      categorie: "",
     },
     {
       autor: "Julie",
       emailAdress: "julie@gmail.com",
       object: "Réunion du lundi",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
       deliveryDate: "10 janv.",
       unRead: true,
       important: false,
@@ -66,13 +94,14 @@ function Home() {
       spam: false,
       pro: false,
       perso: false,
-      categorie: '',
+      categorie: "",
     },
     {
       autor: "Mr smith",
       emailAdress: "smith@gmail.com",
       object: "Votre coli est en chemin",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae conse",
       deliveryDate: "05 janv.",
       unRead: true,
       important: false,
@@ -80,32 +109,27 @@ function Home() {
       spam: false,
       pro: false,
       perso: false,
-      categorie: '',
+      categorie: "",
     },
-  ]
+  ];
 
-  const fullMailToDisplay = useSelector((state) => state.mailDisplayer.value)
-  console.log('useselector', fullMailToDisplay)
+  const fullMailToDisplay = useSelector((state) => state.mailDisplayer.value);
+  console.log("useselector", fullMailToDisplay);
 
-  const displayedMail = <FullMail {...fullMailToDisplay} />
+  const displayedMail = <FullMail {...fullMailToDisplay} />;
 
   const inbox = mailList.map((mail, i) => {
-    return <RowMail key={i} {...mail}
-    />
+    return <RowMail key={i} {...mail} />;
   });
-
 
   return (
     <div>
       <div className={styles.main}>
         {/* header------------------------------- */}
         <header className={styles.header}>
-
           <div className={styles.headerLeft}>
             <div className={styles.divBarsIcon}>
-              <FontAwesomeIcon icon={faBars}
-                className={styles.iconBars} />
-
+              <FontAwesomeIcon icon={faBars} className={styles.iconBars} />
             </div>
 
             <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png" />
@@ -114,8 +138,10 @@ function Home() {
           <div className={styles.headerMid}>
             <div className={styles.searchBar}>
               <div className={styles.iconsRight}>
-                <FontAwesomeIcon icon={faMagnifyingGlass}
-                  className={styles.iconsItems} />
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className={styles.iconsItems}
+                />
               </div>
               <input
                 className={styles.searchInput}
@@ -123,28 +149,32 @@ function Home() {
                 placeholder="Rechercher dans les messages"
               />
               <div className={styles.iconsRight}>
-                <FontAwesomeIcon icon={faSliders}
-                  className={styles.iconsItems} />
+                <FontAwesomeIcon
+                  icon={faSliders}
+                  className={styles.iconsItems}
+                />
               </div>
             </div>
           </div>
 
           <div className={styles.headerRight}>
             <div className={styles.iconsRight}>
-              <FontAwesomeIcon icon={faCircleInfo}
-                className={styles.iconsItems} />
+              <FontAwesomeIcon
+                icon={faCircleInfo}
+                className={styles.iconsItems}
+              />
             </div>
             <div className={styles.iconsRight}>
-              <FontAwesomeIcon icon={faGear}
-                className={styles.iconsItems} />
+              <FontAwesomeIcon icon={faGear} className={styles.iconsItems} />
             </div>
             <div className={styles.iconsRight}>
-              <FontAwesomeIcon icon={faCircleChevronDown}
-                className={styles.iconsItems} />
+              <FontAwesomeIcon
+                icon={faCircleChevronDown}
+                className={styles.iconsItems}
+              />
             </div>
             <div className={styles.iconsRight}>
-              <FontAwesomeIcon icon={faUser}
-                className={styles.iconsItems} />
+              <FontAwesomeIcon icon={faUser} className={styles.iconsItems} />
             </div>
           </div>
         </header>
@@ -155,7 +185,8 @@ function Home() {
           <div className={styles.leftPanel}>
             <div className={styles.newMessageBtn}>
               <div>
-                <FontAwesomeIcon icon={faPen}
+                <FontAwesomeIcon
+                  icon={faPen}
                   className={styles.iconNewMessageBtn}
                 />
                 <span>Nouveau message</span>
@@ -165,18 +196,16 @@ function Home() {
             {/* left selectors primary --------------------- */}
             <div className={styles.sideNavigator}>
               <div className={styles.primaryLeftSelectorsContainer}>
-
                 <div
                   className={styles.leftTabs}
-                  onClick={() => { dispatch(resetMailToDisplay()) }}
+                  onClick={() => {
+                    dispatch(resetMailToDisplay());
+                  }}
                 >
-                  <div
-                    className={styles.tabLabelIcon}
-                  >
-                    <div
-                      className={styles.leftTabsIconContainer}
-                    >
-                      <FontAwesomeIcon icon={faInbox}
+                  <div className={styles.tabLabelIcon}>
+                    <div className={styles.leftTabsIconContainer}>
+                      <FontAwesomeIcon
+                        icon={faInbox}
                         className={styles.iconLeftTab}
                       />
                     </div>
@@ -188,7 +217,8 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faStar}
+                      <FontAwesomeIcon
+                        icon={faStar}
                         className={styles.iconLeftTab}
                       />
                     </div>
@@ -199,7 +229,8 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faClock}
+                      <FontAwesomeIcon
+                        icon={faClock}
                         className={styles.iconLeftTab}
                       />
                     </div>
@@ -210,7 +241,8 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faBookmark}
+                      <FontAwesomeIcon
+                        icon={faBookmark}
                         className={styles.iconLeftTab}
                       />
                     </div>
@@ -221,7 +253,8 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faFile}
+                      <FontAwesomeIcon
+                        icon={faFile}
                         className={styles.iconLeftTab}
                       />
                     </div>
@@ -232,7 +265,8 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faAngleRight}
+                      <FontAwesomeIcon
+                        icon={faAngleRight}
                         className={styles.iconLeftTab}
                       />
                     </div>
@@ -243,23 +277,23 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faAngleRight}
+                      <FontAwesomeIcon
+                        icon={faAngleRight}
                         className={styles.iconLeftTab}
                       />
                     </div>
                     Plus
                   </div>
                 </div>
-
               </div>
 
               {/* left selectors secondary --------------------- */}
               <div className={styles.secondaryLeftSelectorsContainer}>
-
                 <div className={styles.secondarySelectorTitle}>
                   Libellés
                   <div className={styles.iconsRight}>
-                    <FontAwesomeIcon icon={faPlus}
+                    <FontAwesomeIcon
+                      icon={faPlus}
                       className={styles.iconLeftTab}
                     />
                   </div>
@@ -268,14 +302,16 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faBookmark}
+                      <FontAwesomeIcon
+                        icon={faBookmark}
                         className={styles.iconLeftTab}
                       />
                     </div>
                     Professionnel
                   </div>
                   <div className={styles.secondTabOptionIcon}>
-                    <FontAwesomeIcon icon={faEllipsisVertical}
+                    <FontAwesomeIcon
+                      icon={faEllipsisVertical}
                       className={styles.iconLeftTab}
                     />
                   </div>
@@ -283,14 +319,16 @@ function Home() {
                 <div className={styles.leftTabs}>
                   <div className={styles.tabLabelIcon}>
                     <div className={styles.leftTabsIconContainer}>
-                      <FontAwesomeIcon icon={faBookmark}
+                      <FontAwesomeIcon
+                        icon={faBookmark}
                         className={styles.iconLeftTab}
                       />
                     </div>
                     Personnel
                   </div>
                   <div className={styles.secondTabOptionIcon}>
-                    <FontAwesomeIcon icon={faEllipsisVertical}
+                    <FontAwesomeIcon
+                      icon={faEllipsisVertical}
                       className={styles.iconLeftTab}
                     />
                   </div>
@@ -301,55 +339,57 @@ function Home() {
 
           {/* right panel --------------------- */}
           <div className={styles.rightPanel}>
-
             {/* inbox header --------------------- */}
             <div className={styles.inboxHeader}>
               <div className={styles.leftControls}>
                 <div className={styles.selectBox}>
                   <div className={styles.checkBox}>
-                    <FontAwesomeIcon icon={faSquare}
-                      className={styles.iconsLeftControl} />
+                    <FontAwesomeIcon
+                      icon={faSquare}
+                      className={styles.iconsLeftControl}
+                    />
                   </div>
                   <div className={styles.iconCaretDown}>
-                    <FontAwesomeIcon icon={faCaretDown}
-                      className={styles.iconsLeftControl} />
+                    <FontAwesomeIcon
+                      icon={faCaretDown}
+                      className={styles.iconsLeftControl}
+                    />
                   </div>
                 </div>
                 <div className={styles.iconsRight}>
-                  <FontAwesomeIcon icon={faRotateRight}
-                    className={styles.iconsLeftControl} />
+                  <FontAwesomeIcon
+                    icon={faRotateRight}
+                    className={styles.iconsLeftControl}
+                  />
                 </div>
                 <div className={styles.iconsRight}>
-                  <FontAwesomeIcon icon={faEllipsisVertical}
-                    className={styles.iconsLeftControl} />
+                  <FontAwesomeIcon
+                    icon={faEllipsisVertical}
+                    className={styles.iconsLeftControl}
+                  />
                 </div>
-
               </div>
             </div>
 
             {/* mails Panel --------------------- */}
             <div className={styles.mailsPanel}>
-
               {/* categories tabs --------------------- */}
               <div className={styles.tabContainer}>
                 <div className={styles.tabItem}>
                   <div className={styles.tabIcon}>
-                    <FontAwesomeIcon icon={faInbox}
-                    />
+                    <FontAwesomeIcon icon={faInbox} />
                   </div>
                   <span>Principale</span>
                 </div>
                 <div className={styles.tabItem}>
                   <div className={styles.tabIcon}>
-                    <FontAwesomeIcon icon={faTag}
-                    />
+                    <FontAwesomeIcon icon={faTag} />
                   </div>
                   <span>Promotions</span>
                 </div>
                 <div className={styles.tabItem}>
                   <div className={styles.tabIcon}>
-                    <FontAwesomeIcon icon={faUserGroup}
-                    />
+                    <FontAwesomeIcon icon={faUserGroup} />
                   </div>
                   <span>Réseaux sociaux</span>
                 </div>
@@ -357,8 +397,11 @@ function Home() {
 
               {/* mails container ---------------------- */}
               <div className={styles.mailsContainer}>
-                {fullMailToDisplay.autor ? <div>{displayedMail}</div> : <div>{inbox}</div>}
-
+                {fullMailToDisplay.autor ? (
+                  <div>{displayedMail}</div>
+                ) : (
+                  <div>{inbox}</div>
+                )}
               </div>
             </div>
           </div>
